c761b6d399e3a73fa2d6c2e4d2552a27cecefd4f
mohan <mohan.jaddu9@gmail.com>
2021-10-15 19:38:31 +0530

    First MDM innovation project

---
+++ src/main/java/com/mdm/innovation/MdminnovationApplication.java
@@ -0,0 +1,13 @@
+package com.mdm.innovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdminnovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdminnovationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdm/innovation/MdminnovationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdm.innovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdminnovationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
